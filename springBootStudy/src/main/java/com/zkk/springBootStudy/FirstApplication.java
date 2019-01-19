cef798eaa759ef2f7a7b93401661252352c313d0
zk753159 <zk753159@126.com>
2019-01-19 13:04:03 +0800

    fix:修改

--- springBootStudy/src/main/java/com/zkk/springBootStudy/FirstApplication.java
+++ springBootStudy/src/main/java/com/zkk/springBootStudy/FirstApplication.java
@@ -2,11 +2,22 @@ package com.zkk.springBootStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstApplication {
-
+	
+//	@Bean
+//	public Runnable createRunnable() {
+//		return new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("spring boot is running");
+//			}
+//		};
+//	}
 	public static void main(String[] args) {
-		SpringApplication.run(FirstApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstApplication.class, args);
+	//	context.getBean(Runnable.class);
 	}
 }
